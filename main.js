20df34ecc41be37b54e2b258b3798d0662d0a394
ShaziaFarheen <40738963+ShaziaFarheen@users.noreply.github.com>
2018-09-20 11:56:51 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%
+         
+   Cookie firstName = new Cookie("first_name", request.getParameter("first_name"));
+   Cookie lastName = new Cookie("last_name", request.getParameter("last_name"));
+   
+   
+   firstName.setMaxAge(60*60*24); 
+   lastName.setMaxAge(60*60*24); 
+   
+
+   response.addCookie( firstName );
+   response.addCookie( lastName );
+%>
+
+<html>
+   <head>
+      <title>Setting Cookies</title>
+   </head>
+   
+   <body>
+      <center>
+         <h1>Setting Cookies</h1>
+      </center>
+      <ul>
+         <li><p><b>First Name:</b>
+            <%= request.getParameter("first_name")%>
+         </p></li>
+         <li><p><b>Last  Name:</b>
+            <%= request.getParameter("last_name")%>
+         </p></li>
+      </ul>
+   
+   </body>
+</html>
